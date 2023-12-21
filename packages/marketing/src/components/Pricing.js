deefc99d7d28acd1e238d138b12cf26e066db941
Terri Regan <7433075+terriregan@users.noreply.github.com>
2023-12-21 07:27:38 -0500

    marketing debug

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        My Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -26,7 +26,7 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   '@global': {
     ul: {
       margin: 0,
@@ -34,31 +34,31 @@ const useStyles = makeStyles((theme) => ({
       listStyle: 'none',
     },
   },
-  toolbar: {
+  'toolbar': {
     flexWrap: 'wrap',
   },
-  toolbarTitle: {
+  'toolbarTitle': {
     flexGrow: 1,
   },
-  link: {
+  'link': {
     margin: theme.spacing(1, 1.5),
   },
-  heroContent: {
+  'heroContent': {
     padding: theme.spacing(8, 0, 6),
   },
-  cardHeader: {
+  'cardHeader': {
     backgroundColor:
       theme.palette.type === 'light'
         ? theme.palette.grey[200]
         : theme.palette.grey[700],
   },
-  cardPricing: {
+  'cardPricing': {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'baseline',
     marginBottom: theme.spacing(2),
   },
-  footer: {
+  'footer': {
     borderTop: `1px solid ${theme.palette.divider}`,
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
@@ -169,7 +169,7 @@ export default function Pricing() {
       {/* End hero unit */}
       <Container maxWidth="md" component="main">
         <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
+          {tiers.map(tier => (
             // Enterprise card is full width at sm breakpoint
             <Grid
               item
@@ -197,7 +197,7 @@ export default function Pricing() {
                     </Typography>
                   </div>
                   <ul>
-                    {tier.description.map((line) => (
+                    {tier.description.map(line => (
                       <Typography
                         component="li"
                         variant="subtitle1"
@@ -227,13 +227,13 @@ export default function Pricing() {
       {/* Footer */}
       <Container maxWidth="md" component="footer" className={classes.footer}>
         <Grid container spacing={4} justifyContent="space-evenly">
-          {footers.map((footer) => (
+          {footers.map(footer => (
             <Grid item xs={6} sm={3} key={footer.title}>
               <Typography variant="h6" color="textPrimary" gutterBottom>
                 {footer.title}
               </Typography>
               <ul>
-                {footer.description.map((item) => (
+                {footer.description.map(item => (
                   <li key={item}>
                     <Link href="#" variant="subtitle1" color="textSecondary">
                       {item}
