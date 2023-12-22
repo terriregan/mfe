72cbdba450e21c034af3d77175735c0d256e9d1c
Terri Regan <7433075+terriregan@users.noreply.github.com>
2023-12-22 07:02:47 -0500

    add debug log

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
+  console.log('MarketingApp');
   const ref = useRef(null);
 
   useEffect(() => {
