f0d353c3049a63aa00aea2d4e1962f9a8921753f
Terri Regan <7433075+terriregan@users.noreply.github.com>
2023-12-22 07:05:09 -0500

    debugging

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,15 +1,16 @@
 import React, { useRef, useEffect } from 'react';
-import { mount } from 'marketing/MarketingApp';
+//import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
   console.log('MarketingApp');
-  const ref = useRef(null);
+  // const ref = useRef(null);
 
-  useEffect(() => {
-    mount(ref.current);
-  }, []);
+  // useEffect(() => {
+  //   mount(ref.current);
+  // }, []);
 
-  return <div ref={ref} />;
+  // return <div ref={ref} />;
+  return <div id="marketing" />;
 };
 
 export default MarketingApp;
